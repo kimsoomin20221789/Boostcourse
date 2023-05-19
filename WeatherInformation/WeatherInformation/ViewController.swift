f68dc6c67c8e0b6720970867cb0f5c5bb131826a
kimsoomin <skno1382@soongsil.ac.kr>
2023-05-20 01:09:18 +0900

    초기화

---
+++ WeatherInformation/WeatherInformation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeatherInformation
+//
+//  Created by 김수민 on 2023/05/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
